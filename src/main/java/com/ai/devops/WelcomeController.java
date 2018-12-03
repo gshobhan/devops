392cb1603b76917c877d2d264f12bbbb660eb8b5
Shobhan Ganta <SGanta@AutomationImage.local>
2018-12-03 15:32:31 -0600

    add logging msg in controller

--- src/main/java/com/ai/devops/WelcomeController.java
+++ src/main/java/com/ai/devops/WelcomeController.java
@@ -1,13 +1,18 @@
 package com.ai.devops;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@RequestMapping("/")
 	public String welcome(Model model) {
+		logger.info("processing index request");
 		model.addAttribute("course", "DevOps");
 		return "index";
 	}
